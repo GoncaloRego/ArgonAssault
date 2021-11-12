963befa6d44753c0943ab5ba971649726d2d266d
GoncaloRego <goncalorego30@gmail.com>
2021-11-12 17:29:13 +0000

    Ship movement control and firing lasers; Ship Timeline complete;

--- Argon Assault/Assets/Scripts/PlayerController.cs
+++ Argon Assault/Assets/Scripts/PlayerController.cs
@@ -4,23 +4,52 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] float controlSpeed;
+    [SerializeField] float xRange;
+    [SerializeField] float yRange;
 
-    void OnEnable()
-    {
-    }
+    [SerializeField] float positionPitchFactor;
+    [SerializeField] float controlPitchFactor;
+    [SerializeField] float positionYawFactor;
+    [SerializeField] float controlRollFactor;
 
-    void OnDisable()
-    {
-    }
+    float horizontal, vertical;
+
+    Transform cachedTransform;
 
     void Start()
     {
-
+        cachedTransform = transform;
     }
 
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        Move();
+        Rotate();
+    }
+
+    void Move()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        float xOffset = horizontal * controlSpeed * Time.deltaTime;
+        float newPosX = cachedTransform.localPosition.x + xOffset;
+        float clampedPosX = Mathf.Clamp(newPosX, -xRange, xRange);
+
+        float yOffset = vertical * controlSpeed * Time.deltaTime;
+        float newPosY = cachedTransform.localPosition.y + yOffset;
+        float clampedPosY = Mathf.Clamp(newPosY, -yRange, yRange);
+
+        cachedTransform.localPosition = new Vector3(clampedPosX, clampedPosY, cachedTransform.localPosition.z);
+    }
+
+    void Rotate()
+    {
+        float pitch = cachedTransform.localPosition.y * positionPitchFactor + vertical * controlPitchFactor;
+        float yaw = cachedTransform.localPosition.x * positionYawFactor;
+        float roll = horizontal * controlRollFactor * Time.deltaTime;
+
+        cachedTransform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 }
